a5b9d9391ae129ff20fbf108f3b14e6f3a8ab614
PETERHUBV <167149712+PETERHUBV@users.noreply.github.com>
2024-09-30 07:31:05 +0800

    Add files via upload

--- Assets/Scenes/Script/Circle Rotation.cs
+++ Assets/Scenes/Script/Circle Rotation.cs
@@ -2,7 +2,7 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class CircleRotation : MonoBehaviour
 {
@@ -11,34 +11,64 @@ public class CircleRotation : MonoBehaviour
     public Transform Rotation;
     public Vector3 DefPos;
     public bool Clockwise = true;
+    public bool stopped = false;
     public float timeelapsed = 0f;
 
 
     // Start is called before the first frame update
 
-
     // Update is called once per frame
     void Update()
     {
+
+        {
+
+        }
 
         timeelapsed += Time.deltaTime;
 
         if (timeelapsed >= switchTime)
         {
-            // Switch the rotation direction
-            Clockwise = !Clockwise;
-
-            // Reset the elapsed time
             timeelapsed = 0f;
         }
+        // Switch the rotation direction
+        if (Input.GetMouseButtonDown(0))
+        {
+            Clockwise = !Clockwise;
+        }
+
+
+
+
 
         float direction = Clockwise ? 1f : -1f;
-       
-        transform.Rotate(Vector3.forward, Circlespeed * direction  * Time.deltaTime);
-       
+
+        transform.Rotate(Vector3.forward, Circlespeed * direction * Time.deltaTime);
+
 
         Debug.Log("Rotation");
-    }
+        void OnTriggerEnter(Collider other)
+        {
 
+
+            if (other.CompareTag("") && !stopped)
+            {
+                StopMovement(other.gameObject);
+            }
+            void StopMovement(GameObject target)
+            {
+               
+                Rigidbody2D rb = target.GetComponent<Rigidbody2D>();
+                if (rb != null)
+                {
+                    rb.velocity = Vector2.zero; 
+                    rb.isKinematic = true; 
+
+                }
+                stopped = true;
+            }
+        }
     }
+}
+    
 
